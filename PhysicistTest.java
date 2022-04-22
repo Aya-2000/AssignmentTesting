5092f6c58036d183b284a15df1761f65422cd34f
Aya-Ahmed-Gamal <19P1689@eng.asu.edu.eg>
2022-04-23 01:46:46 +0200

    adding new TC

--- PhysicistTest.java
+++ PhysicistTest.java
@@ -9,7 +9,7 @@ public class PhysicistTest {
     int [][]arr1={{-1,-2,3},{-2,3,-1},{3,-1,-2}};
     int [][]arr2={{-1,2,3},{1,-2,-3}};
     int [][]arr3={{-200,100,300},{20,30,25},{-20,-20,-20}};
-    
+    int [][]arr4={{-100,100,100},{60,30,25}};
 
 
     @Test
@@ -22,6 +22,8 @@ public class PhysicistTest {
         assertEquals(true, ph.Coordinates(2, arr2));
 
         assertEquals(false, ph.Coordinates(3, arr3));
+
+         assertEquals(false, ph.Coordinates(2, arr4));
 
 
     }
